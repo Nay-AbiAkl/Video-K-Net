19ad1ac63af09d4fd82f6fe6b8f822787b290401
mariam-hegazy <103334171+mariam-hegazy@users.noreply.github.com>
2023-04-23 21:35:38 +0200

    change SyncBN to BN

--- configs/det/knet_cityscapes_step/knet_s3_r50_fpn.py
+++ configs/det/knet_cityscapes_step/knet_s3_r50_fpn.py
@@ -1,8 +1,8 @@
 _base_ = [
-    '../_base_/schedules/schedule_1x.py',
-    '../_base_/default_runtime.py',
-    '../_base_/models/knet_citystep_s3_r50_fpn.py',
-    '../_base_/datasets/cityscapes_step.py',
+    "../_base_/schedules/schedule_1x.py",
+    "../_base_/default_runtime.py",
+    "../_base_/models/knet_citystep_s3_r50_fpn.py",
+    "../_base_/datasets/cityscapes_step.py",
 ]
 
 
@@ -10,25 +10,27 @@ num_proposals = 100
 # load_from = "/mnt/lustre/lixiangtai/pretrained/video_knet_vis/knet_r50_city.pth"
 load_from = None
 
-work_dir = 'logger/blackhole'
+work_dir = "logger/blackhole"
 
-runner = dict(type='EpochBasedRunner', max_epochs=8)
+runner = dict(type="EpochBasedRunner", max_epochs=8)
 
 model = dict(
-    type='KNet',
+    type="KNet",
     backbone=dict(
-        type='ResNet',
+        type="ResNet",
         depth=50,
         num_stages=4,
         out_indices=(0, 1, 2, 3),
         frozen_stages=1,
-        norm_cfg=dict(type='SyncBN', requires_grad=True),
+        norm_cfg=dict(type="BN", requires_grad=True),
         norm_eval=True,
-        style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        style="pytorch",
+        init_cfg=dict(type="Pretrained", checkpoint="torchvision://resnet50"),
+    ),
     roi_head=dict(
-            type='KernelIterHead',
-            merge_joint=True,),
+        type="KernelIterHead",
+        merge_joint=True,
+    ),
     test_cfg=dict(
         rpn=None,
         rcnn=dict(
@@ -37,15 +39,22 @@ model = dict(
             stuff_score_thr=0.05,
             merge_stuff_thing=dict(
                 overlap_thr=0.6,
-                iou_thr=0.5, stuff_max_area=4096, instance_score_thr=0.3)))
+                iou_thr=0.5,
+                stuff_max_area=4096,
+                instance_score_thr=0.3,
+            ),
+        ),
+    ),
 )
 
 lr_config = dict(
-    policy='step',
-    warmup='linear',
+    policy="step",
+    warmup="linear",
     warmup_iters=1000,
     warmup_ratio=0.001,
-    step=[7, ],
+    step=[
+        7,
+    ],
 )
 data = dict(
     samples_per_gpu=2,
