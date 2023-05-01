1f30d3e61edc4fc7c4a344100e2a4af21e471a71
Nay-AbiAkl <nayabiakl@gmail.com>
2023-05-01 09:58:22 +0200

    fixing prepare paths

--- scripts/kitti_step_prepare.py
+++ scripts/kitti_step_prepare.py
@@ -6,8 +6,8 @@ val_seqs = [2, 6, 7, 8, 10, 13, 14, 16, 18]
 test_seqs = list(range(29))
 
 # your download the KITTI STEP dataset.
-data_root = os.path.expanduser('/home/nabiakl/kitti-step/training/')
-data_root_test = os.path.expanduser('/home/nabiakl/kitti-step/testing/')
+data_root = os.path.expanduser('/home/nabiakl/kitti-step-2/training/')
+data_root_test = os.path.expanduser('/home/nabiakl/kitti-step-2/testing/')
 data_out = os.path.expanduser('/home/nabiakl/kitti_out')
 
 
