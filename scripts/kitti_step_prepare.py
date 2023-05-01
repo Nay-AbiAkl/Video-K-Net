4c160d29b0df10395fcc38b54201bf30d28f6b6c
Nay-AbiAkl <nayabiakl@gmail.com>
2023-05-01 08:48:15 +0200

    changing kitti-step prepare paths

--- scripts/kitti_step_prepare.py
+++ scripts/kitti_step_prepare.py
@@ -6,9 +6,9 @@ val_seqs = [2, 6, 7, 8, 10, 13, 14, 16, 18]
 test_seqs = list(range(29))
 
 # your download the KITTI STEP dataset.
-data_root = os.path.expanduser("/home/nabiakl/Video-K-Net/data/kitti-step/video_sequence/train/")
-data_root_test = os.path.expanduser("/home/nabiakl/Video-K-Net/data/kitti-step/video_sequence/test/")
-data_out = os.path.expanduser('/data/data1/datasets/STEP/kitti_out')
+data_root = os.path.expanduser('/home/nabiakl/kitti-step/training/')
+data_root_test = os.path.expanduser('/home/nabiakl/kitti-step/testing/')
+data_out = os.path.expanduser('/home/nabiakl/kitti_out')
 
 
 def build_panoptic(seq_id, input_dir, output_dir):
